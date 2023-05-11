e8bae4a7e573692819bc18506f97b46215761263
Melissa Evangelista <melissaevangelista52@gmail.com>
2023-05-10 22:07:42 -0400

    added all header elements

--- src/index.js
+++ src/index.js
@@ -1,42 +1,74 @@
-const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-  },
-  "main-content": {
-    "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4": "About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "services-h4": "Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4": "Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4": "Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4": "Contact",
-    "address": "123 Way 456 Street Somewhere, USA",
-    "phone": "1 (888) 888-8888",
-    "email": "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright": "Copyright Great Idea! 2021",
-  },
-  "images": {
-    "logo-img": "http://localhost:9000/img/logo.png",
-    "cta-img": "http://localhost:9000/img/cta.png",
-    "accent-img": "http://localhost:9000/img/accent.png",
-  },
+const siteContent = {
+	// DO NOT CHANGE THIS OBJECT
+	nav: {
+		"nav-item-1": "Services",
+		"nav-item-2": "Product",
+		"nav-item-3": "Vision",
+		"nav-item-4": "Features",
+		"nav-item-5": "About",
+		"nav-item-6": "Contact",
+	},
+	cta: {
+		h1: "DOM Is Awesome",
+		button: "Get Started",
+	},
+	"main-content": {
+		"features-h4": "Features",
+		"features-content":
+			"Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"about-h4": "About",
+		"about-content":
+			"About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"services-h4": "Services",
+		"services-content":
+			"Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"product-h4": "Product",
+		"product-content":
+			"Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+		"vision-h4": "Vision",
+		"vision-content":
+			"Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+	},
+	contact: {
+		"contact-h4": "Contact",
+		address: "123 Way 456 Street Somewhere, USA",
+		phone: "1 (888) 888-8888",
+		email: "sales@greatidea.io",
+	},
+	footer: {
+		copyright: "Copyright Great Idea! 2021",
+	},
+	images: {
+		"logo-img": "http://localhost:9000/img/logo.png",
+		"cta-img": "http://localhost:9000/img/cta.png",
+		"accent-img": "http://localhost:9000/img/accent.png",
+	},
 };
 
-console.log('project wired!')
+console.log("project wired!");
+
+const nav = document.querySelector("nav");
+const navLinks = nav.getElementsByTagName("a");
+const objNav = siteContent.nav;
+
+console.log(navLinks);
+
+// Setting the class names
+let index = 0;
+for (let aTag of navLinks) {
+	aTag.className = "italic";
+	index += 1;
+}
+
+let servicesNav = (nav.children[0].innerHTML = "Services");
+let productNav = (nav.children[1].innerHTML = "Product");
+let visionNav = (nav.children[2].innerHTML = "Vision");
+let featuresNav = (nav.children[3].innerHTML = "Features");
+let aboutNav = (nav.children[4].innerHTML = "About");
+let contactNav = (nav.children[5].innerHTML = "Contact");
+
+const logoImg = document.getElementById("logo-img");
+logoImg.setAttribute("src", "http://localhost:9000/img/logo.png");
+console.log(logoImg);
+
+//end
